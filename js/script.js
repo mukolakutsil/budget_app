fc1a9df1a2888847155632a7c76124d29572410f
Mukola <mukola.kutsil22@gmail.com>
2020-02-13 13:42:49 +0200

    add_element_on_js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var btnStart = document.querySelector(".start"),
+    resultTtable = document.querySelector('.result-table'),
+
+    budgetValue = document.querySelector(".budget-value"),
+    daybudgetValue = document.querySelector(".daybudget-value"),
+    levelValue = document.querySelector("level-value"),
+    expensesValue = document.querySelector("expenses-value"),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector(".income-value"),
+    monthsavingsValue = document.querySelector(".monthsavings-value"),
+    yearSavingsValue = document.querySelector(".yearsavings-value"),
+    expensesItem  = document.getElementsByClassName("expenses-item"),
+    btnr = document.getElementsByTagName("button"),
+    btnr0 = btnr[0],
+    btnr1 = btnr[1],
+    btnr2 = btnr[2],
+    optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    expensesItem = document.querySelectorAll(".expenses-item"),
+    chooseIncome = document.querySelector(".choose-income"),
+    savings = document.querySelector("#savings"),
+    chooseSum = document.querySelector(".choose-sum"),
+    choosePercent = document.querySelector(".choose-percent"),
+    yearValue = document.querySelector(".year-value"),
+    monthValue = document.querySelector(".month-value"),
+    dayValue = document.querySelector(".day-value");
+
+
+
+
+    
+
+
+console.log(btnStart);
+console.log(resultTtable);
+console.log(expensesItem[2]);
+console.log(btnr2);
+console.log(dayValue);
+
+
+
+
